bbf9f1b82e97298a24669211ec5bc5134c9d6266
Victor77-svg <77808602+Victor77-svg@users.noreply.github.com>
2021-03-20 11:17:16 -0300

    Update donut.js

--- donut.js
+++ donut.js
@@ -1,45 +1,133 @@
-$("#chart-container").insertFusionCharts({
-  type: "doughnut2d",
-  width: "100%",
-  height: "100%",
-  dataFormat: "json",
-  dataSource: {
-    chart: {
-      caption: "Android Distribution for our app",
-      enableSmartLabels: false,
-      subcaption: "For all users in 2017",
-      showpercentvalues: "1",
-      defaultcenterlabel: "Android Distribution",
-      aligncaptionwithcanvas: "0",
-      captionpadding: "0",
-      decimals: "0",
-      plottooltext:
-        "<b>$percentValue</b> of our Android users are on <b>$label</b>",
-      
-      theme: "fusion"
-    },
-    data: [
-      {
-        color: "#29577b",
-        label: "Ice Cream Sandwich",
-        value: "18900"
-      },
-      {
-        color:"#35c09c",
-        label: "Jelly Bean",
-        value: "5300"
-      },
-      {
-        color:"#f6ce49",
-        label: "Kitkat",
-        value: "10500"
-      },
-      {
-        color:"#f7a35c",
-        label: "Lollipop",
-        value: "1900"
+// Seed data to populate the donut pie chart
+var seedData = [{
+  "label": "React",
+  "value": 25,
+  "link": "https://facebook.github.io/react/"
+}, {
+  "label": "Redux",
+  "value": 25,
+  "link": "https://redux.js.org/"
+}, {
+  "label": "Vue.js",
+  "value": 25,
+  "link": "https://vuejs.org/"
+}, {
+  "label": "AngularJS",
+  "value": 25,
+  "link": "https://angularjs.org/"
+}, {
+  "label": "Meteor",
+  "value": 25,
+  "link": "https://meteorhacks.com/meteor-js-web-framework-for-everyone"
+}, {
+  "label": "Node.js",
+  "value": 25,
+  "link": "https://nodejs.org/"
+}];
+
+// Define size & radius of donut pie chart
+var width = 450,
+    height = 450,
+    radius = Math.min(width, height) / 2;
+
+// Define arc colours
+var colour = d3.scaleOrdinal(d3.schemeCategory20);
+
+// Define arc ranges
+var arcText = d3.scaleOrdinal()
+  .range([0, width]);
+
+// Determine size of arcs
+var arc = d3.arc()
+  .innerRadius(radius - 130)
+  .outerRadius(radius - 10);
+
+// Create the donut pie chart layout
+var pie = d3.pie()
+  .value(function (d) { return d["value"]; })
+  .sort(null);
+
+// Append SVG attributes and append g to the SVG
+var svg = d3.select("#donut-chart")
+  .attr("width", width)
+  .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(" + radius + "," + radius + ")");
+
+// Define inner circle
+svg.append("circle")
+  .attr("cx", 0)
+  .attr("cy", 0)
+  .attr("r", 100)
+  .attr("fill", "#fff") ;
+
+// Calculate SVG paths and fill in the colours
+var g = svg.selectAll(".arc")
+  .data(pie(seedData))
+  .enter().append("g")
+  .attr("class", "arc")
+		
+  // Make each arc clickable 
+  .on("click", function(d, i) {
+    window.location = seedData[i].link;
+  });
+
+	// Append the path to each g
+	g.append("path")
+  	.attr("d", arc)
+  	.attr("fill", function(d, i) {
+    	return colour(i);
+  	});
+
+	// Append text labels to each arc
+	g.append("text")
+  	.attr("transform", function(d) {
+    	return "translate(" + arc.centroid(d) + ")";
+  	})
+  	.attr("dy", ".35em")
+  	.style("text-anchor", "middle")
+  	.attr("fill", "#fff")
+		.text(function(d,i) { return seedData[i].label; })
+  
+g.selectAll(".arc text").call(wrap, arcText.range([0, width]));
+
+// Append text to the inner circle
+svg.append("text")
+  .attr("dy", "-0.5em")
+  .style("text-anchor", "middle")
+  .attr("class", "inner-circle")
+  .attr("fill", "#36454f")
+  .text(function(d) { return 'JavaScript'; });
+
+svg.append("text")
+  .attr("dy", "1.0em")
+  .style("text-anchor", "middle")
+  .attr("class", "inner-circle")
+  .attr("fill", "#36454f")
+  .text(function(d) { return 'is lots of fun!'; });
+
+// Wrap function to handle labels with longer text
+function wrap(text, width) {
+  text.each(function() {
+    var text = d3.select(this),
+        words = text.text().split(/\s+/).reverse(),
+        word,
+        line = [],
+        lineNumber = 0,
+        lineHeight = 1.1, // ems
+        y = text.attr("y"),
+        dy = parseFloat(text.attr("dy")),
+        tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+    console.log("tspan: " + tspan);
+    while (word = words.pop()) {
+      line.push(word);
+      tspan.text(line.join(" "));
+      if (tspan.node().getComputedTextLength() > 90) {
+        line.pop();
+        tspan.text(line.join(" "));
+        line = [word];
+        tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
       }
-      
-    ]
-  }
-});
+    }
+  });
+}
